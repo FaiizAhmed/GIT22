6a802eaa2e95d450888251a32075823806dd609d
FaiizAhmed <faiiz.ahmed@gmail.com>
2024-07-22 23:57:39 -0500

    second commit

--- src/main/java/GitClass.java
+++ src/main/java/GitClass.java
@@ -2,5 +2,9 @@ public class GitClass {
 
     public static void main(String[] args) {
         System.out.println("Hello GIT");
+        System.out.println("Git add ");
+        System.out.println("Git commit");
+
+
     }
 }
