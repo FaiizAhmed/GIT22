c4df4807a7674d5e8e3d3b3abf51299fb6aa21dc
FaiizAhmed <faiiz.ahmed@gmail.com>
2024-08-12 02:30:25 -0500

    Added message from UI

--- src/main/java/GitClass.java
+++ src/main/java/GitClass.java
@@ -4,7 +4,8 @@ public class GitClass {
         System.out.println("Hello GIT");
         System.out.println("Git add ");
         System.out.println("Git commit");
-
-
+        System.out.println("Git push");
+        System.out.println("new change");
+        System.out.println("new project UI");
     }
 }
